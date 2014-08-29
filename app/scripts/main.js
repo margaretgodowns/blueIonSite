6ea7f122866a57b717432e9925045497a17ae6cd
Margaret Godowns <margaretgodowns@gmail.com>
2014-08-29 15:02:42 -0400

    the final countdown

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,59 @@ $(document).ready(function(){
 
   $(".blogSection").find("a").click(function(){
     event.preventDefault();
-    $(".blogSection").siblings().toggleClass("hidden");
+    $(this).closest(".blogSection").siblings().toggleClass("hidden");
+    $(this).closest(".blogSection").toggleClass("expand");
 
-    //if/then statement?
+
+  });
+
+  $(document).ready(function() {
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $(".container-fluid").position();
+        if (scroll > topDist.top) {
+            $('.topMenu').css({"position":"fixed","top":"0"});
+        } else {
+            $('.topMenu').css({"position":"static","top":"auto", "z-index": "500"});
+        }
+    });
+});
+
+  $(".sk8tNews").click(function(){
+    event.preventDefault();
+    console.log("working");
+    $("html, body").animate({
+      scrollTop: $(".news").offset().top
+    } )
+  });
+
+  $(".skateBoarders").click(function(){
+    event.preventDefault();
+    console.log("working");
+    $("html, body").animate({
+      scrollTop: $(".skateboarders").offset().top
+    } )
+  });
+
+  $(".boardDesign").click(function(){
+    event.preventDefault();
+    console.log("working");
+    $("html, body").animate({
+      scrollTop: $(".boards").offset().top
+    } )
+  });
+
+  $(".contact").click(function(){
+    event.preventDefault();
+    console.log("working");
+    $("html, body").animate({
+      scrollTop: $(".contactUs").offset().top
+    } )
   });
 
 
   $(".skateboard").tooltip({
     "placement":"top",
+    "html": true,
   });
 });
